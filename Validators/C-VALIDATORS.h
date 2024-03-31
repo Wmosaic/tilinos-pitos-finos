3a3fc2e32993d5ddbe4558cf6af12553c8451ab3
Wmosaic <153867300+Wmosaic@users.noreply.github.com>
2024-03-30 18:39:41 -0600

    Creación cabecera validadores c++
    Se crea el archivo de cabecera en c++, hecho en teléfono,extremar precauciones. Incompleto.

---
+++ Validators/C-VALIDATORS.h
@@ -0,0 +1,9 @@
+#ifndef VALIDATORS
+#define VALIDATORS
+
+
+class Validators:
+    private:
+        boolean isNum(auto test);
+        boolean isNom(auto test);
+        boolean isDate(String test);
