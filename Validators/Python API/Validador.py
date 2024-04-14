c0850b3cc0b3ff29d6bb499b07a350ea4050ac2d
werfenGranate45 <eduardo.agustin.cervantes@gmail.com>
2024-04-13 20:14:24 -0600

    Mejoramiento de Estilo
    Simplemente mejore el estilo del código
    
    Pero me gustaría en este commit decir que usare la clase Auxiliar PruebasClases.py para hacer el testing no estará en el producto final (por lo tanto ignorar)

--- Validators/Python API/Validador.py
+++ Validators/Python API/Validador.py
@@ -5,20 +5,11 @@ class Validador:
     def __init__(self) -> None:
         pass
     
-    """
-    * El metodo formatearCadenas Exclusivo de Python
-    * Cuando copias una ruta de acceso viene con '"' 
-    * Entoces lo quitamos para poder trabajar la ruta de acceso
-    * El caracter '\\' es equivalente a '\', que es el caracter que 
-    * Que da problemas a Python a la hora de trabajar rutas de acceso
-    """
-    
     def formatearCadena(self, cadena) -> str:
         if '"' in cadena:
             direcFormateada = (cadena.replace('"','')).replace('\\','/')
         else:
             direcFormateada = cadena.replace('\\','/')
-        
         return direcFormateada
     
     def isNum(self, cadena) -> bool:
@@ -27,7 +18,6 @@ class Validador:
             return True
         except: return False
     
-    #Exis
     def isNom(self, cadena) -> bool:
         #El metodo rsplit genera una lista de strings eliminando espacios
         aux = True
@@ -40,15 +30,16 @@ class Validador:
         else: return False
         return aux
     
+    #Dependiendo de la longitud de la fecha capturada se extree el año 
+    #Para verificar si el año es mayor a 1900, usando el slicig
     def isDate(self, fecha) -> bool:
         try:
             datetime.strptime(fecha, '%d/%m/%Y')
-            #Dependiendo de la longitud de la fecha capturada se extree el año 
-            # para verificar si el año es mayor a 1900, usando el slicig
             match len(fecha):
                 case 10: return int(fecha[6:10]) >= 1900 
-                case 9:  return int(fecha[5:9]) >= 1900
-                case 8:  return int(fecha[4:8]) >= 1900
+                case 9: return int(fecha[5:9]) >= 1900
+                case 8: return int(fecha[4:8]) >= 1900
+                case _: return False
         except: return False
     
     def isDir(self, cadena) -> bool:  
@@ -59,17 +50,15 @@ class Validador:
         except OSError: return False
         except UnicodeDecodeError: return False
     
-   
     def isStrVacio(self, cadena) -> bool:
-       if isinstance(cadena, str):
-           return len(cadena) == 0
-       elif cadena == None:
-           return True
+       if isinstance(cadena, str): return len(cadena) == 0
+       elif cadena == None: return True
        else: return False
     
     #La direccion ya debe estar validada responsabilidad del programador
-    #Nota: Creo que no se desarrollada en C++ ni en Java 
-    def valCSV(self, direccion):
+    #Nota: Creo que no se desarrollada en C++ ni en Java por lo tanto es metodo dispar
+    #Entre los demas lenguajes hasta ahora
+    def valCSV(self, direccion) -> None:
          rowError,cuenta = 1,0
          with open(direccion,encoding='UTF-8') as csvFile: 
             csvFile.readline()
