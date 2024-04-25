80094c839ad4ed4b814837c8739a07f460a1c0c7
werfenGranate45 <eduardo.agustin.cervantes@gmail.com>
2024-04-25 10:05:24 -0600

    [Wip] CLASE ADMISTRADOR HERENCIA

---
+++ 20A40/cir-agus/EJER_34/Administrador.h
@@ -0,0 +1,17 @@
+# include <string>
+
+# define ADMINISTRADOR_H
+# ifdef ADMINISTRADOR_H
+
+class Administrador
+{
+private:
+    /* data */
+public:
+    Administrador(/* args */);
+    ~Administrador();
+};
+
+
+
+#endif
